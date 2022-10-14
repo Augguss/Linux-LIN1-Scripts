d41f181f2e0301a6a7313f521bf122d38afeceb2
DavidVarosoGomesCPNV <48351164+DavidVarosoGomesCPNV@users.noreply.github.com>
2022-10-14 10:16:43 +0200

    Add files via upload

--- Test.sh
+++ Test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-IP="10.10.10.55"
-hostname="SRV-LIN1-02"
+declare IP="10.10.10.55"
+declare hostname="SRV-LIN1-02"
 
 
-echo "Starting Script . . ."
-echo "$IP"
-echo "$hostname"
+echo Starting Script . . .
+echo $IP
+echo $hostname
