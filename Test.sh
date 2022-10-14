2afbb6079bb2eb0339d5f03da91d621538e7abf7
DavidVarosoGomesCPNV <48351164+DavidVarosoGomesCPNV@users.noreply.github.com>
2022-10-14 10:45:56 +0200

    Add files via upload

--- Test.sh
+++ Test.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-declare IP="10.10.10.55"
+declare IP="10.10.10.110"
 declare hostname="SRV-LIN1-02"
+declare gateway="10.10.10.2"
+declare interface="
+# This file describes the network interfaces available on your system/n
+# and how to activate them. For more information, see interfaces(5)./n
+
+source /etc/network/interfaces.d/*/n
+
+# The loopback network interface/n
+auto lo/n
+iface lo inet loopback/n
+
+allow-hotplug ens32/n
+iface ens32 inet static/n
+address 10.10.10.11/n
+netmask 255.255.255.0/n
+gateway 10.10.10.2/n"
 
 
 echo Starting Script . . .
@@ -11,3 +27,5 @@ echo $hostname
 sudo hostname $hostname
 sudo echo $hostname > /etc/hostname
 sudo echo $hostname > /etc/hosts
+
+sudo echo $interface > /etc/network/interfaces
