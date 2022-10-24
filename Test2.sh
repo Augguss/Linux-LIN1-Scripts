206ab87e8d2de31292133cbf04fade99c28e42ac
DavidVarosoGomesCPNV <48351164+DavidVarosoGomesCPNV@users.noreply.github.com>
2022-10-24 12:48:55 +0200

    Add files via upload

--- Test2.sh
+++ Test2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare IP="10.10.10.110"
+declare IP="10.10.10.115"
 declare hostname="SRV-LIN1-02"
 declare gateway="10.10.10.2"
 
@@ -12,18 +12,25 @@ sudo hostname $hostname
 sudo echo $hostname > /etc/hostname
 sudo echo $hostname > /etc/hosts
 
-sudo bash -c 'cat > /etc/network/interfaces << EOF 
+net_FILE="/etc/network/interfaces"
+cat <<EOM >$net_FILE
+
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
 source /etc/network/interfaces.d/*
 
-# The loopback network interface is SCRIPTED
+# The loopback network interface is scripted !
 auto lo
 iface lo inet loopback
 
-allow-hotplug ens32
-iface ens32 inet static
-address 10.10.10.110
+# The primary network interface
+auto ens33
+iface ens33 inet static
+address 10.10.10.11
 netmask 255.255.255.0
 gateway 10.10.10.2
-EOF'
+
+EOM
 
 sudo echo All good !
