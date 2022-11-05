da429c2a740345335bd3f5f6ddba0c43d81c66b8
DavidVarosoGomesCPNV <48351164+DavidVarosoGomesCPNV@users.noreply.github.com>
2022-11-05 13:45:42 +0100

    Add files via upload

---
+++ NAS-LIN1-01.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#Config LIN1-SRV-02
+
+declare IP="10.10.10.33"
+declare hostname="NAS-LIN1-01"
+declare gateway="10.10.10.2"
+declare mask="255.255.255.0"
+declare domain="lin1.local"
+
+echo Starting Script . . .
+echo $IP
+echo $hostname
+echo $gateway
+echo $mask
+echo $domain
+
+#hostname config 
+
+sudo hostname $hostname
+sudo echo $hostname > /etc/hostname
+sudo echo $hostname > /etc/hosts
+
+#IP config
+
+net_FILE="/etc/network/interfaces"
+cat <<EOM >$net_FILE
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+
+auto lo
+iface lo inet loopback
+
+# The primary network interface is scripted with variables !
+auto ens32
+iface ens32 inet static
+address $IP
+netmask $mask
+gateway $gateway
+EOM
+
+#DNS et Gateway Config
+name_FILE="/etc/resolv.conf"
+cat <<EOM >$name_FILE
+#Scripted
+domain $domain
+search $domain
+nameserver 10.10.10.11
+nameserver 10.10.10.2
+EOM
+
+sudo echo All good !
